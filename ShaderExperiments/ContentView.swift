8912d07edbebcb5acfc1477e7396b89431744477
Mark Feaver <mark.feaver@gmail.com>
2024-12-21 08:28:25 +1100

    Add recolour shader

--- ShaderExperiments/ContentView.swift
+++ ShaderExperiments/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
                 .clipped()
                 .foregroundStyle(.tint)
                 .colorEffect(
-                    ShaderLibrary.passthrough()
+                    ShaderLibrary.recolour()
                 )
         }
         .padding()
--- ShaderExperiments/Shaders.metal
+++ ShaderExperiments/Shaders.metal
@@ -4,3 +4,7 @@ using namespace metal;
 [[ stitchable ]] half4 passthrough(float2 position, half4 color) {
     return color;
 }
+
+[[ stitchable ]] half4 recolour(float2 position, half4 color) {
+    return half4(1, 0, 0, color.a);
+}
